d1059a59398939ee210830eff8b8e13cbbecaf7d
Marc Solé <msolefonte@gmail.com>
2022-08-06 17:28:03 +0200

    Update 1.2.0 (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,21 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.0.0] Initial Release - 29.07.2022
+## [1.0.0] - 29.07.2022
 
 - Initial Release
 
-## [1.1.0] Minor Update - 30.07.2022
+## [1.1.0] - 30.07.2022
 
 - Add support for the Enable AI Confederation flag (MCT)
 - Fix a bug that allowed non-loreful confederations
+
+## [1.2.0] - 06.08.2022
+
+- Major rework
+  - Almost all code has been refactored
+  - Dataset is now static and has been externalized
+  - Should fix the non-loreful confederations bug
+- Add a log file for easier and cleaner bug reporting
+- Rename the MCT defalt section to "Base Options"
+- Update CHANGELOG.md format
---
+++ src/script/campaign/mod/wolfy_chaos_confederation_dataset.lua
@@ -0,0 +1,187 @@
+local wcc_dataset = {
+  confederable_factions = {
+    placeholder_archaon_faction = {
+      "placeholder_festus_faction",
+      "placeholder_azazel_faction",
+      "placeholder_sigvald_faction",
+      "placeholder_kholek_faction",
+      "placeholder_valkia_faction",
+      "placeholder_vilitch_faction"
+    },
+    placeholder_festus_faction = {
+      "wh3_main_nur_poxmakers_of_nurgle",
+      "wh3_main_nur_bubonic_swarm",
+      "wh3_main_nur_maggoth_kin",
+      "wh3_main_nur_septic_claw"
+    },
+    placeholder_azazel_faction = {
+      "wh3_main_sla_seducers_of_slaanesh",
+      "placeholder_sigvald_faction",
+      "wh3_main_sla_exquisite_pain",
+      "wh3_main_sla_rapturous_excess",
+      "wh3_main_sla_subtle_torture"
+    },
+    placeholder_sigvald_faction = {
+      "wh3_main_sla_seducers_of_slaanesh",
+      "placeholder_azazel_faction",
+      "wh3_main_sla_exquisite_pain",
+      "wh3_main_sla_rapturous_excess",
+      "wh3_main_sla_subtle_torture"
+    },
+    placeholder_valkia_faction = {
+      "wh3_main_kho_exiles_of_khorne",
+      "wh3_main_kho_brazen_throne",
+      "wh3_main_kho_crimson_skull",
+      "wh3_main_kho_karneths_sons"
+    },
+    placeholder_vilitch_faction = {
+      "wh3_main_tze_oracles_of_tzeentch",
+      "wh3_main_tze_all_seeing_eye",
+      "wh3_main_tze_broken_wheel",
+      "wh3_main_tze_flaming_scribes",
+      "wh3_main_tze_sarthoraels_watchers"
+    },
+    wh3_main_nur_poxmakers_of_nurgle = {
+      "placeholder_festus_faction",
+      "wh3_main_nur_bubonic_swarm",
+      "wh3_main_nur_maggoth_kin",
+      "wh3_main_nur_septic_claw"
+    },
+    wh3_main_sla_seducers_of_slaanesh = {
+      "placeholder_azazel_faction",
+      "placeholder_sigvald_faction"
+    },
+    wh3_main_kho_exiles_of_khorne = {
+      "placeholder_valkia_faction",
+      "wh3_main_kho_brazen_throne",
+      "wh3_main_kho_crimson_skull",
+      "wh3_main_kho_karneths_sons"
+    },
+    wh3_main_tze_oracles_of_tzeentch = {
+      "placeholder_vilitch_faction",
+      "wh3_main_tze_all_seeing_eye",
+      "wh3_main_tze_broken_wheel",
+      "wh3_main_tze_flaming_scribes",
+      "wh3_main_tze_sarthoraels_watchers"
+    },
+    placeholder_belakor_faction = {
+      "wh3_main_nur_poxmakers_of_nurgle",
+      "wh3_main_nur_bubonic_swarm",
+      "wh3_main_nur_maggoth_kin",
+      "wh3_main_nur_septic_claw",
+      "wh3_main_sla_seducers_of_slaanesh",
+      "wh3_main_sla_exquisite_pain",
+      "wh3_main_sla_rapturous_excess",
+      "wh3_main_sla_subtle_torture",
+      "placeholder_kholek_faction",
+      "wh3_main_kho_exiles_of_khorne",
+      "wh3_main_kho_brazen_throne",
+      "wh3_main_kho_crimson_skull",
+      "wh3_main_kho_karneths_sons",
+      "wh3_main_tze_oracles_of_tzeentch",
+      "wh3_main_tze_all_seeing_eye",
+      "wh3_main_tze_broken_wheel",
+      "wh3_main_tze_flaming_scribes",
+      "wh3_main_tze_sarthoraels_watchers"
+    },
+    wh3_main_dae_daemon_prince = {
+      "wh3_main_nur_poxmakers_of_nurgle",
+      "wh3_main_nur_bubonic_swarm",
+      "wh3_main_nur_maggoth_kin",
+      "wh3_main_nur_septic_claw",
+      "wh3_main_sla_seducers_of_slaanesh",
+      "wh3_main_sla_exquisite_pain",
+      "wh3_main_sla_rapturous_excess",
+      "wh3_main_sla_subtle_torture",
+      "placeholder_kholek_faction",
+      "wh3_main_kho_exiles_of_khorne",
+      "wh3_main_kho_brazen_throne",
+      "wh3_main_kho_crimson_skull",
+      "wh3_main_kho_karneths_sons",
+      "wh3_main_tze_oracles_of_tzeentch",
+      "wh3_main_tze_all_seeing_eye",
+      "wh3_main_tze_broken_wheel",
+      "wh3_main_tze_flaming_scribes",
+      "wh3_main_tze_sarthoraels_watchers"
+    }
+  },
+  confederable_factions_no_minor = {
+    placeholder_archaon_faction = {
+      "placeholder_festus_faction",
+      "placeholder_azazel_faction",
+      "placeholder_sigvald_faction",
+      "placeholder_kholek_faction",
+      "placeholder_valkia_faction",
+      "placeholder_vilitch_faction"
+    },
+    placeholder_festus_faction = {
+      "wh3_main_nur_poxmakers_of_nurgle"
+    },
+    placeholder_azazel_faction = {
+      "wh3_main_sla_seducers_of_slaanesh",
+      "placeholder_sigvald_faction"
+    },
+    placeholder_sigvald_faction = {
+      "wh3_main_sla_seducers_of_slaanesh",
+      "placeholder_azazel_faction"
+    },
+    placeholder_valkia_faction = {
+        "wh3_main_kho_exiles_of_khorne"
+    },
+    placeholder_vilitch_faction = {
+      "wh3_main_tze_oracles_of_tzeentch"
+    },
+    wh3_main_nur_poxmakers_of_nurgle = {
+      "placeholder_festus_faction"
+    },
+    wh3_main_sla_seducers_of_slaanesh = {
+      "placeholder_azazel_faction",
+      "placeholder_sigvald_faction"
+    },
+    wh3_main_kho_exiles_of_khorne = {
+      "placeholder_valkia_faction"
+    },
+    wh3_main_tze_oracles_of_tzeentch = {
+      "placeholder_vilitch_faction"
+    },
+    placeholder_belakor_faction = {
+      "wh3_main_nur_poxmakers_of_nurgle",
+      "wh3_main_sla_seducers_of_slaanesh",
+      "placeholder_kholek_faction",
+      "wh3_main_kho_exiles_of_khorne",
+      "wh3_main_tze_oracles_of_tzeentch"
+    },
+    wh3_main_dae_daemon_prince = {
+      "wh3_main_nur_poxmakers_of_nurgle",
+      "wh3_main_sla_seducers_of_slaanesh",
+      "placeholder_kholek_faction",
+      "wh3_main_kho_exiles_of_khorne",
+      "wh3_main_tze_oracles_of_tzeentch"
+    }
+  },
+  confederable_legendary_lords = {
+    wh_dlc01_chs_kholek_suneater = true,
+    wh_dlc01_chs_prince_sigvald = true,
+    placeholder_azazel = true,
+    placeholder_festus = true,
+    placeholder_vilitch = true,
+    placeholder_valkia = true,
+    wh3_main_nur_kugath = true,
+    wh3_main_sla_nkari = true,
+    wh3_main_tze_kairos = true,
+    wh3_main_kho_skarbrand = true
+  },
+  confederable_races = {
+    "wh_main_chs_chaos",
+    "wh3_main_dae_daemons",
+    "wh3_main_kho_khorne",
+    "wh3_main_nur_nurgle",
+    "wh3_main_sla_slaanesh",
+    "wh3_main_tze_tzeentch"
+  },
+  dilemma_execution_option = 1,
+  dilemma_key = "wolfy_roc_confederation_generic",
+  dilemma_key_lls = "wolfy_roc_confederation_generic_no_execution"
+}
+
+return wcc_dataset;
