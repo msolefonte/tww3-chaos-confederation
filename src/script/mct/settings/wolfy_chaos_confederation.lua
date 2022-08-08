dd88378add2d05d12ff96db02ee6f5b1a1534d01
Marc Solé <msolefonte@gmail.com>
2022-08-08 21:01:50 +0200

    1.2.1

--- src/script/mct/settings/wolfy_chaos_confederation.lua
+++ src/script/mct/settings/wolfy_chaos_confederation.lua
@@ -8,9 +8,16 @@ mct_mod:set_title("Allegiance of the Damned");
 mct_mod:set_author("Wolfy");
 mct_mod:set_description("Allow Chaos factions to confederate via imposition following loreful rules.");
 
-local mct_section_base = mct_mod:add_new_section("1-wcc-base", "Base Options");
+mct_mod:add_new_section("1-wcc-base", "Base Options");
 
 local wcc_enable_minor_factions = mct_mod:add_new_option("wcc_enable_minor_factions", "checkbox");
 wcc_enable_minor_factions:set_default_value(true);
 wcc_enable_minor_factions:set_text("Enable Minor Factions");
 wcc_enable_minor_factions:set_tooltip_text("Enable if you want minor factions to be confederable");
+
+mct_mod:add_new_section("2-wcc-ao", "Advanced Options", false);
+
+local wcc_logging_enabled = mct_mod:add_new_option("wcc_logging_enabled", "checkbox");
+wcc_logging_enabled:set_default_value(false);
+wcc_logging_enabled:set_text("Enable logging");
+wcc_logging_enabled:set_tooltip_text("If enabled, a log will be populated as you play. Use it to report bugs!");
